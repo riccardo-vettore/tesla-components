3377d331d09ccd7f54f677ee6f66e77e566cad76
riccardovettore <riccardo.vettore.dev@gmail.com>
2023-02-14 23:02:44 +0100

    Insert missing exports in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './tesla-button/tesla-button';
 export * from './tesla-home-section/tesla-home-section';
+export * from './tesla-navbar/tesla-navbar';
+export * from './tesla-navbar-item/tesla-navbar-item';
